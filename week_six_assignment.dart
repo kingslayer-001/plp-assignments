6b8532eafe933c87db933242c4ad4db19cfd4b82
kingslayer-001 <jeffmk.dev@google.com>
2024-06-06 17:44:48 +0300

    Assignment

---
+++ week_six_assignment.dart
@@ -0,0 +1,31 @@
+main() {
+  //1. A function that takes in two integers and outputs the sum
+  int addNum(int num1, int num2) {
+    return num1 + num2;
+  }
+  //2. a program that uses a for loop to print out the numbers from 1 to 10.
+
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+  //3. a program that uses a switch statement to check for different string values and output a response based on the value.
+
+  //4. a program that uses a while loop to print out the numbers from 20 to 10.
+  int num = 20;
+  while (num < 10) {
+    print(num);
+    num--;
+  }
+
+  //5. a program that uses an if-else statement to check if a number is even or odd and output the result.
+  int number = 5;
+  if (number % 2 == 0) {
+    print('The number is even');
+  } else {
+    print('The number is odd');
+  }
+
+//6. a program that takes a list of numbers as input and outputs the largest number in the list.
+
+//7. a program that uses a try-catch block to catch an exception and output an error message.
+}
